6aa1ad946d1f59189a4eb659bc0f37a2f7d57021
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2022-09-09 22:49:46 +0100

    initial commit

---
+++ src/Hello.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Hello = () => {
+    return <div>Lokomotive</div>;
+}
+
+export default Hello;
---
+++ src/app.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import Hello from './Hello';
+
+function render() {
+    ReactDOM.render(<Hello />, document.body);
+}
+
+render();
